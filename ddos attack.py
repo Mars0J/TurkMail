3d19037191f3677b7771ed7cea4bd5242a01093a
Mars0J <70023143+Mars0J@users.noreply.github.com>
2020-11-01 18:44:34 +0300

    ddos.attack.py

---
+++ ddos attack.py
@@ -0,0 +1,19 @@
+import socket 
+import random 
+import os 
+
+os.system("clear") 
+banner="""
+##################################
+
+Ayyıldız Airbob Ddos Attack V1.0 #      
+
+Code by Ayyıldız Airbob          # 
+                                                                                       
+##################################         
+
+
+"""
+print("banner") 
+
+
